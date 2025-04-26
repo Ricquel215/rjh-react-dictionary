578dfc232fc831bb4137ba40e1f50f24680648d2
Ricquel215 <ricqueljh215@gmail.com>
2025-04-26 13:08:40 -0500

    Setup search engine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello</header>
+      <div>
+        <header className="App-header"></header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Ricquel Harper</small>
+        </footer>
+      </div>
     </div>
   );
 }

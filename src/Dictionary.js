b006553d5648fe014a3d67af451d7fe2e02ed358
Ricquel215 <ricqueljh215@gmail.com>
2025-04-26 13:58:38 -0500

    Corrected code

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -6,28 +6,25 @@ export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
   function handleResponse(response) {
-    console.log(response);
-
-    function search(event) {
-      event.preventDefault();
-      alert(`Searching for ${keyword} definition`);
-
-      let apiKey = "74bb1a547f9eao9e3t800fc79244b60b";
-      let apiUrl =
-        "https://api.shecodes.io/dictionary/v1/define?word={word}&key={apiKey}";
-      axios.get(apiUrl).then(handleResponse);
-    }
-
-    function handleKeywordChange(event) {
-      setKeyword(event.target.value);
-    }
-
-    return (
-      <div className="Dictionary">
-        <form onSubmit={search}>
-          <input type="search" onChange={handleKeywordChange} />
-        </form>
-      </div>
-    );
+    console.log(response.data[0]);
   }
+
+  function search(event) {
+    event.preventDefault();
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" onChange={handleKeywordChange} />
+      </form>
+    </div>
+  );
 }
